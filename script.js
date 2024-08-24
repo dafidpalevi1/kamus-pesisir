e667056b68d575ae165d25aa0a979b87bbd64326
dafid palevi <43629045+dafidpalevi1@users.noreply.github.com>
2024-08-25 05:12:46 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -28,6 +28,8 @@ document.addEventListener('DOMContentLoaded', function() {
             'sedang': 'sadang',
             'kesana': 'kasinun',
             'sama': 'samo',
+            'tinggal': 'tingga',
+            'orang': 'urang',
         };
 
         // Pisahkan input menjadi kata-kata
